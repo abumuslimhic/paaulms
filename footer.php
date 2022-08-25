08b8be0cfefd758b61594a6b3d2eef8602ed9b07
Abdussalam HIC <63915319+abumuslimhic@users.noreply.github.com>
2022-08-25 11:56:03 +0100

    first commit

---
+++ footer.php
@@ -0,0 +1,17 @@
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,700">
+		<link rel="stylesheet" type="text/css" href="css/footer_style.css" />
+	</head>
+	<body>
+		<footer>
+				<div id="cd-logo">
+					<p>
+                        &copy 19CS1023 FINAL YEAR PROJECT <br>
+                        PRINCE ABUBAKAR AUDU UNIVERSITY (PAAU), ANYIGBA | ALL RIGHTS RESERVED <br>
+                        <script>document.write(new Date ().getFullYear())</script>.
+                    </p>
+                </div>
+        </footer>
+	</body>
+</html>
